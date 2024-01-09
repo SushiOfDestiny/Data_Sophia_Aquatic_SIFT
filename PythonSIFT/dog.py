9d77ab1d5ebac1284283ffcb467cac1d8e74c590
Matthieu Colin <matthieu.colin@etu.minesparis.psl.eu>
2024-01-09 12:12:27 +0100

    Add colormap uniformization

--- PythonSIFT/dog.py
+++ PythonSIFT/dog.py
@@ -2,22 +2,26 @@ from pysift import generateBaseImage, computeNumberOfOctaves, generateGaussianKe
 import matplotlib.pyplot as plt
 from cv2 import imread, imshow, waitKey
 import logging
+import numpy as np
 
 def visualize_DoG(
     image, sigma=1.6, num_intervals=3, assumed_blur=0.5, image_border_width=5
 ):
-    imshow("image", image)
-    k = waitKey()
     image = image.astype("float32")
     base_image = generateBaseImage(image, sigma, assumed_blur)
     num_octaves = computeNumberOfOctaves(base_image.shape)
     gaussian_kernels = generateGaussianKernels(sigma, num_intervals)
     gaussian_images = generateGaussianImages(base_image, num_octaves, gaussian_kernels)
     dog_images = generateDoGImages(gaussian_images)
-    fig, ax = plt.subplots(dog_images.shape[0], dog_images.shape[1])
+    fig, ax = plt.subplots(dog_images.shape[0], 1)
+    vmin = np.min([np.min([[np.min(x) for x in m] for m in dog_images])])
+    vmax = np.max([np.max([[np.max(x) for x in m] for m in dog_images])])
     for i in range(dog_images.shape[0]):
-        for j in range(dog_images.shape[1]):
-            ax[i, j].imshow(dog_images[i, j])
+        dog_row = np.concatenate(dog_images[i, :], axis=1)
+        im = ax[i].imshow(dog_row, vmin=vmin, vmax=vmax, cmap='gray')
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    fig.colorbar(im, cax=cbar_ax)
     plt.show()
 
 if __name__ == '__main__':
