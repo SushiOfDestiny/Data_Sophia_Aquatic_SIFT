e649a2d95c67f2c3ea55a5360785f3695532ce80
Jules Royer <jules.royer@etu.minesparis.psl.eu>
2024-01-09 13:21:14 +0100

    changed in dog but reversed

--- PythonSIFT/dog.py
+++ PythonSIFT/dog.py
@@ -1,7 +1,18 @@
-from pysift import generateBaseImage, computeNumberOfOctaves, generateGaussianKernels, generateGaussianImages, generateDoGImages
+import sys, os
+
+from pysift import (
+    generateBaseImage,
+    computeNumberOfOctaves,
+    generateGaussianKernels,
+    generateGaussianImages,
+    generateDoGImages,
+)
 import matplotlib.pyplot as plt
+
 from cv2 import imread, imshow, waitKey
+
 import logging
+
 
 def visualize_DoG(
     image, sigma=1.6, num_intervals=3, assumed_blur=0.5, image_border_width=5
@@ -20,6 +31,20 @@ def visualize_DoG(
             ax[i, j].imshow(dog_images[i, j])
     plt.show()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logger = logging.getLogger(__name__)
-    visualize_DoG(imread('box.png', 0))
+    visualize_DoG(imread("box.png", 0))
+
+    # # visualize for aqua picture
+    # # load image from absolute path
+
+    # script_dir = sys.path[0]
+    # aqua_image_path = "../discover_opencv/images/aqua_images/"
+    # image_name = "87_img_"
+    # image_extension = ".png"
+    # img_path = os.path.join(script_dir, aqua_image_path + image_name + image_extension)
+    # # img_path = aqua_image_path + image_name + image_extension
+
+    # # load image
+    # imread(img_path, 0)
