507a5265aa6a883030908a0dfe86efb15e17153e
Jules Royer <jules.royer@etu.minesparis.psl.eu>
2024-01-09 14:23:42 +0100

    visualization functions and tests

--- PythonSIFT/visu_extrema.py
+++ PythonSIFT/visu_extrema.py
@@ -1,3 +1,5 @@
+import os
+
 import logging
 import pysift
 
@@ -35,6 +37,7 @@ from numpy import (
 
 from numpy.linalg import det, lstsq, norm
 
+import matplotlib.pyplot as plt
 
 ####################
 # Global variables #
@@ -45,9 +48,16 @@ float_tolerance = 1e-7
 
 
 def compute_fitted_extrema(
-    image, sigma=1.6, num_intervals=3, assumed_blur=0.5, image_border_width=5
+    image,
+    sigma=1.6,
+    num_intervals=3,
+    assumed_blur=0.5,
+    image_border_width=5,
+    contrast_threshold=0.04,
+    eigenvalue_ratio=10,
 ):
-    """Compute SIFT extrema after quadratic fitting"""
+    """Compute SIFT extrema after quadratic fitting.
+    Current version allows to modify the contrast threshold and the edge threshold."""
     image = image.astype("float32")
     base_image = pysift.generateBaseImage(image, sigma, assumed_blur)
     num_octaves = pysift.computeNumberOfOctaves(base_image.shape)
@@ -56,8 +66,15 @@ def compute_fitted_extrema(
         base_image, num_octaves, gaussian_kernels
     )
     dog_images = pysift.generateDoGImages(gaussian_images)
+
     keypoints = pysift.findScaleSpaceExtrema(
-        gaussian_images, dog_images, num_intervals, sigma, image_border_width
+        gaussian_images,
+        dog_images,
+        num_intervals,
+        sigma,
+        image_border_width,
+        contrast_threshold,
+        eigenvalue_ratio,
     )
 
     return keypoints
@@ -141,4 +158,67 @@ def compute_raw_extrema(
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
-    print("oui")
+    # print current working directory
+    print(os.getcwd())
+
+    # load image
+    image = cv.imread("triangle.jpg", 0)
+
+    # parameters
+    sigma = 1.6
+    num_intervals = 3
+    assumed_blur = 0.5
+    image_border_width = 5
+    contrast_threshold = 0.04
+    eigenvalue_ratio = 10
+
+    # compute fitted extrema
+    keypoints = compute_fitted_extrema(
+        image,
+        sigma,
+        num_intervals,
+        assumed_blur,
+        image_border_width,
+        contrast_threshold,
+        eigenvalue_ratio,
+    )
+
+    nb_keypoints = len(keypoints)
+
+    # draw keypoints
+    kp_color = (255, 0, 0)  # red
+
+    kp_image = cv.drawKeypoints(
+        image,
+        keypoints,
+        None,
+        color=kp_color,
+        flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+    )
+
+    # show image
+    plt.imshow(kp_image, cmap="gray")
+    # write parameters on image
+    plt.text(
+        10,
+        10,
+        "number of keypoints = "
+        + str(nb_keypoints)
+        + "\nsigma = "
+        + str(sigma)
+        + "\nnum_intervals = "
+        + str(num_intervals)
+        + "\nassumed_blur = "
+        + str(assumed_blur)
+        + "\nimage_border_width = "
+        + str(image_border_width)
+        + "\ncontrast_threshold = "
+        + str(contrast_threshold)
+        + "\neigenvalue_ratio = "
+        + str(eigenvalue_ratio),
+        color="white",
+        fontsize=8,
+        bbox=dict(facecolor="black", alpha=0.5),
+    )
+    plt.title("Fitted Pysift extrema")
+    plt.show()
