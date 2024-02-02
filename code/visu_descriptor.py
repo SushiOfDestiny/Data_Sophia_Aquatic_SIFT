9e008e944ddf2d73ff747e018f7f54b79ce7b857
Jules Royer <jules.royer@etu.minesparis.psl.eu>
2024-02-02 12:36:28 +0100

    added full keypoint visualization pipeline

--- code/visu_descriptor.py
+++ code/visu_descriptor.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 from scipy.signal import convolve, fftconvolve
 import scipy.ndimage as ndimage
 
+import visu_hessian as vh
+import descriptor as desc
+
 ############################
 # Descriptor Visualization #
 ############################
@@ -37,17 +40,17 @@ def display_spatial_histograms(histograms, title="Spatial Histograms"):
     # loop over all subplots
     for bin_j in range(nb_bins):
         for bin_i in range(nb_bins):
+            # get the current subplot
+            ax = axs[bin_j, bin_i] if nb_bins > 1 else axs[bin_i]
+
             # display the histogram
-            axs[bin_j, bin_i].bar(
-                angles,
-                histograms[bin_j, bin_i, :],
-            )
+            ax.bar(angles, histograms[bin_j, bin_i, :])
+
             # set the title of the subplot
-            axs[bin_j, bin_i].set_title(
-                f"Bin ({bin_j}, {bin_i})",
-            )
+            ax.set_title(f"Bin ({bin_j}, {bin_i})")
+
             # add axis labels
-            axs[bin_j, bin_i].set_xlabel("Angle (degrees)")
+            ax.set_xlabel("Angle (degrees)")
 
     # set the title of the figure
     fig.suptitle(title)
@@ -102,19 +105,24 @@ def display_matched_histograms(
     for id_hist in range(2):
         for bin_j in range(nb_bins):
             for bin_i in range(nb_bins):
-                # display the histogram
+                # calculate column id
                 col_id = id_hist * nb_bins + bin_i
-                axs[bin_j, col_id].bar(
+
+                # get the current subplot
+                ax = axs[bin_j, col_id] if nb_bins > 1 else axs[col_id]
+
+                # display the histogram
+                ax.bar(
                     angles,
                     hists[id_hist][bin_j, bin_i, :],
                     color=colors[id_hist],
                 )
                 # set the title of the subplot
-                axs[bin_j, col_id].set_title(
+                ax.set_title(
                     f"Bin ({bin_j}, {bin_i})",
                 )
                 # add axis labels
-                axs[bin_j, col_id].set_xlabel("Angle (degrees)")
+                ax.set_xlabel("Angle (degrees)")
 
     # set the title of the figure
     fig.suptitle(hist_title)
@@ -126,3 +134,71 @@ def display_matched_histograms(
     plt.subplots_adjust(hspace=0.5, wspace=0.5)
 
     # plt.show()
+
+
+###########################
+# Visualisation Pipelines #
+###########################
+
+
+def descriptor_visualisation_pipeline(
+    kps,
+    float_ims,
+    nb_bins=3,
+    bin_radius=2,
+    border_size=1,
+    delta_angle=5.0,
+    sigma=0,
+    normalization_mode="global",
+):
+    """
+    Compute and display the descriptor of keypoints side by side
+    """
+    # get keypoints positions
+    kps_pos = [tuple(np.round(kp.pt).astype(int)) for kp in kps]
+
+    # compute neighborhood radius
+    neigh_radius = (2 * bin_radius + 1) * nb_bins // 2
+    larger_neigh_radius = neigh_radius + 3 * border_size
+
+    # crop the subimages around keypoints
+    crop_ims = [
+        vh.crop_image_around_keypoint(float_ims[i], kps_pos[i], larger_neigh_radius)
+        for i in range(2)
+    ]
+
+    # compute zoomed positions
+    zoomed_pos = [larger_neigh_radius, larger_neigh_radius]
+
+    # compute overall features of the neighborhoods
+    zoomed_features = [
+        desc.compute_features_overall_abs(crop_ims[i], border_size) for i in range(2)
+    ]
+
+    descriptor_histograms_kps = [
+        desc.compute_descriptor_histograms_1_2_rotated(
+            overall_features_1_2=zoomed_features[i],
+            kp_position=zoomed_pos,
+            nb_bins=nb_bins,
+            bin_radius=bin_radius,
+            delta_angle=delta_angle,
+            sigma=sigma,
+            normalization_mode=normalization_mode,
+        )
+        for i in range(2)
+    ]
+
+    values_names = [
+        "first principal direction",
+        "second principal direction",
+        "gradient directions",
+    ]
+
+    for id_feature in range(3):
+        display_matched_histograms(
+            hist1=descriptor_histograms_kps[0][id_feature],
+            hist2=descriptor_histograms_kps[1][id_feature],
+            hist_title=values_names[id_feature],
+        )
+
+    plt.show()
