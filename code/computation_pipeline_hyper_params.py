5cd46f49986924d186b3f2fd5cf467af9c404a98
Matthieu Colin <matthieu.colin@etu.minesparis.psl.eu>
2024-02-08 11:54:13 +0100

    Add parallelization in nested descriptor computation loop

--- code/computation_pipeline_hyper_params.py
+++ code/computation_pipeline_hyper_params.py
@@ -43,46 +43,46 @@
 # epsilon = None
 
 
-##############
-# Pipeline 2 #
-##############
+# ##############
+# # Pipeline 2 #
+# ##############
 
-# define images to load
-relative_path = "../data"
+# # define images to load
+# relative_path = "../data"
+# # img_folder = "blender/rocks"
 # img_folder = "blender/rocks"
-img_folder = "blender/rocks"
-photo_name = "rock_1"
-im_name1 = "left"
-im_name2 = "right"
-im_names = (im_name1, im_name2)
-im_ext = "png"
+# photo_name = "rock_1"
+# im_name1 = "left"
+# im_name2 = "right"
+# im_names = (im_name1, im_name2)
+# im_ext = "png"
 
-# set the coordinates of the subimages
-y_starts = [530, 560]
-y_lengths = [10, 10]
-x_starts = [1200, 1200]
-x_lengths = [10, 10]
+# # set the coordinates of the subimages
+# y_starts = [530, 560]
+# y_lengths = [10, 10]
+# x_starts = [1200, 1200]
+# x_lengths = [10, 10]
 
-# define sigma for the gaussian blur
-blur_sigma = 1.0
+# # define sigma for the gaussian blur
+# blur_sigma = 1.0
 
-# define the border size
-border_size = 1
+# # define the border size
+# border_size = 1
 
-# define descriptor parameters
-nb_bins = 3
-bin_radius = 2
-delta_angle = 5.0
-sigma = 0
-normalization_mode = "global"
-neigh_radius = (2 * bin_radius + 1) * nb_bins // 2 + bin_radius
-nb_angular_bins = int(360.0 / delta_angle) + 1
+# # define descriptor parameters
+# nb_bins = 3
+# bin_radius = 2
+# delta_angle = 5.0
+# sigma = 0
+# normalization_mode = "global"
+# neigh_radius = (2 * bin_radius + 1) * nb_bins // 2 + bin_radius
+# nb_angular_bins = int(360.0 / delta_angle) + 1
 
-# set the distance type, between "min" and "all"
-distance_type = "min"
+# # set the distance type, between "min" and "all"
+# distance_type = "min"
 
-# define the blender filtering precision threshold
-epsilon = None
+# # define the blender filtering precision threshold
+# epsilon = None
 
 
 # ##############
@@ -169,3 +169,44 @@ epsilon = None
 
 # # define the blender filtering precision threshold
 # epsilon = None
+
+##############
+# Pipeline 5 #
+##############
+
+# define images to load
+relative_path = "../data"
+# img_folder = "blender/rocks"
+img_folder = "blender/rocks"
+photo_name = "rock_1"
+im_name1 = "left"
+im_name2 = "right"
+im_names = (im_name1, im_name2)
+im_ext = "png"
+
+# set the coordinates of the subimages
+y_starts = [530, 560]
+y_lengths = [25, 25]
+x_starts = [1200, 1200]
+x_lengths = [25, 25]
+
+# define sigma for the gaussian blur
+blur_sigma = 1.0
+
+# define the border size
+border_size = 1
+
+# define descriptor parameters
+nb_bins = 3
+bin_radius = 2
+delta_angle = 5.0
+sigma = 0
+normalization_mode = "global"
+neigh_radius = (2 * bin_radius + 1) * nb_bins // 2 + bin_radius
+nb_angular_bins = int(360.0 / delta_angle) + 1
+
+# set the distance type, between "min" and "all"
+distance_type = "min"
+
+# define the blender filtering precision threshold
+epsilon = None
