e903d998b42ed7212994d2958a04e3bc00c3a77b
Matthieu Colin <matthieu.colin@etu.minesparis.psl.eu>
2024-02-12 15:29:44 +0100

    Add final pipeline

--- code/final_pipeline.py
+++ code/final_pipeline.py
@@ -1,30 +1,11 @@
 import subprocess
 import os
 from computation_pipeline_hyper_params import *
+from filenames_creation import *
 import argparse
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--hmd", help="launch homemade descriptor pipeline", action="store_true")
-    parser.add_argument("--sift", help="launch SIFT pipeline", action="store_true")
-    parser.add_argument("--filt", help="prefilter possible keypoints before descriptor calculation", action="store_true")
-    args = parser.parse_args()
-
-    if args.filt and not args.hmd:
-        raise ValueError("Cannot launch prefiltering without homemade descriptor")
-
-    if args.sift:
-        os.environ["sift"] = True
-        from filenames_creation import *
-        subprocess.call(["python", "general_pipeline_before_blender.py", "--sift"])
-        out = subprocess.check_output(f'blender {original_imgs_path_prefix}/{blender_filename}.blend -b -P filter_matches.py', shell=True)
-        print(out)
-        subprocess.call(["python", "general_pipeline_after_blender.py", "--sift"])
-
-    if args.hmd:
-        os.environ["sift"] = False
-        from filenames_creation import *
-        subprocess.call(["python", "general_pipeline_before_blender.py"])
-        out = subprocess.check_output(f'blender {original_imgs_path_prefix}/{blender_filename}.blend -b -P filter_matches.py', shell=True)
-        print(out)
-        subprocess.call(["python", "general_pipeline_after_blender.py"])
+    subprocess.call(["python", "general_pipeline_before_blender.py"])
+    out = subprocess.check_output(f'blender {original_imgs_path_prefix}/{blender_filename}.blend -b -P filter_matches.py', shell=True)
+    print(out)
+    subprocess.call(["python", "general_pipeline_after_blender.py"])
