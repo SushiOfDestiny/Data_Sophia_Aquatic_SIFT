9aadb1d85a177050ce4da70659f41f292c7e836c
Matthieu Colin <matthieu.colin@etu.minesparis.psl.eu>
2024-02-13 09:35:03 +0100

    logs

--- code/final_pipeline.py
+++ code/final_pipeline.py
@@ -2,7 +2,12 @@ import subprocess
 import os
 from computation_pipeline_hyper_params import *
 from filenames_creation import *
-import argparse
+from general_pipeline_before_blender import logger
+# if __name__ == '__main__':
+#     subprocess.call(["python", "general_pipeline_before_blender.py"])
+#     out = subprocess.check_output(f'blender {original_imgs_path_prefix}/{blender_filename}.blend -b -P filter_matches.py', shell=True)
+#     print(out)
+#     subprocess.call(["python", "general_pipeline_after_blender.py"])
 
 if __name__ == '__main__':
     subprocess.call(["python", "general_pipeline_before_blender.py"])
